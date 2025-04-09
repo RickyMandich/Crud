2290e38eb6527f8196d39c9c93ab35dd86a78e5d
RickyMandich <ricky.mandich@gmail.com>
2025-04-09 12:29:31 +0200

    aggiornamento 2025 04 09 12:29

--- src/main/java/it/fermi/controller/crud/FindCluster.java
+++ src/main/java/it/fermi/controller/crud/FindCluster.java
@@ -6,30 +6,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class FindCluster{
     @GetMapping("/findCluster")
-    public List<String> findCluster(@RequestParam (required = false) String clusterCode) {
-        List<String> ret = new java.util.ArrayList<String>();
+    public List<List<String>> findCluster(@RequestParam (required = false) String clusterCode) {
+        List<List<String>> ret = new java.util.ArrayList<List<String>>();
         Connection conn = DBEntry.getConnection();
         if (conn == null) {
-            ret.add("impossibile recuperare la connessione");
+            ret.add(new ArrayList<String>(List.of("impossibile stabilire una connessione")));
             return ret;
         }
         try (java.sql.Statement stmt = conn.createStatement()) {
             clusterCode = (clusterCode == null) ? "" : clusterCode;
             clusterCode = "%" + clusterCode + "%";
             String select = "select * from spool_cluster where cluster_code like '" + clusterCode + "';";
-            ret.add(select);
+            ret.add(new ArrayList<String>(List.of(select)));
             try (java.sql.ResultSet rs = stmt.executeQuery(select)) {
                 while (rs.next()){
-                    ret.add(rs.getString("cluster_code"));
+                    List<String> res = new ArrayList<String>();
+                    for(int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                        res.add(rs.getString(i+1));
+                    }
+                    ret.add(res);
                 }
             }
         } catch (java.sql.SQLException e) {
-            ret.add("impossibile recuperare i dati");
+            ret.add(new ArrayList<>(List.of("impossibile recuperare i dati")));
             e.printStackTrace();
         }
         return ret;
