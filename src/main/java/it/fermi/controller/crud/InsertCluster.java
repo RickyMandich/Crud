34d0ae2bf42802aa60082eb4c07af91199c57af8
RickyMandich <ricky.mandich@gmail.com>
2025-04-09 15:06:48 +0200

    aggiornamento 2025 04 09 15:06

--- src/main/java/it/fermi/controller/crud/InsertCluster.java
+++ src/main/java/it/fermi/controller/crud/InsertCluster.java
@@ -1,18 +1,16 @@
 package it.fermi.controller.crud;
 
 import it.fermi.utility.DBEntry;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class InsertCluster{
-    @GetMapping("/insertCluster")
+    @PostMapping("/insertCluster")
     public List<String> insertCluster(
             @RequestParam String clusterCode,
             @RequestParam (required = false) String description,
